7f25750f2e491b05bc4a6eb8a2f12a69403f7516
alexandermeves01 <alexandermeves01@gmail.com>
2020-06-11 10:24:04 -0400

    Fixed Hello World for working with VSC Ubuntu instead of Eclipse Che

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,11 @@
 
 class HelloWorld {
+
     public static void main (String args[] ) {
         System.out.println("Hello World");
         System.out.println("Butternut Squash");
     }
+
 }
 
+    
